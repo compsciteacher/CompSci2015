8195b89bf7c17fd7047f062a05e097911b242946
CSA-HAA <1178@chaparralstaracademy.com>
2015-09-18 10:27:36 -0500

    HAA

---
+++ Date/src/Main.java
@@ -0,0 +1,24 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		String day, month;
+		day = "Thursday";
+		month = "September";
+		int date,  year;
+		date = 17;
+		year = 2015;
+		System.out.println("American Format:");
+		System.out.print(day + ", ");
+		System.out.print(month + " ");
+		System.out.print(date + ", ");
+		System.out.println(year);
+		System.out.println("European Format:");
+		System.out.print(day + " ");
+		System.out.print(date + " ");
+		System.out.print(month + ", ");
+		System.out.println(year);
+
+	}
+}
