17d5820228aca8c1c00a40cd7ca0ee680427f1d7
Collin Patterson <0019@chaparralstaracademy.com>
2015-09-21 22:51:19 -0500

    test

---
+++ COMPSCI2_PM/VariablesCDP/src/StillUsingVariables.java
@@ -0,0 +1,9 @@
+public class StillUsingVariables
+{
+	public static void main( String[] args )
+	{
+String myName = "Collin Patterson ";
+String myGrad = "2018.";
+System.out.println( "My name is " + myName + "and I'll graduate in " + myGrad);
+	}
+}
