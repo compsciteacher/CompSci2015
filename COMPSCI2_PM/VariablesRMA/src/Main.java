40f831c38b68fc843e5723ae7aa528aa432f30d8
HCD <howardcdavis@hotmail.com>
2015-09-24 10:18:23 -0500

    move and create answer folders

---
+++ COMPSCI2_PM/VariablesRMA/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		String name="Reggie";
+		String loc="Chaparral Star Academy";
+		String date="September 11th";
+		int age=9090900;
+		System.out.println(name);
+		System.out.println(loc);
+		System.out.println(age);
+		System.out.println("");
+		hello(name,loc,age);
+	}
+	public static void hello(String newname, String newloc, int newage)
+	{
+		newname="John";
+		newloc="Nowhere";
+		newage=7;
+		System.out.print("Howdy, "+newname+newloc+newage+"!");
+	}
+}
